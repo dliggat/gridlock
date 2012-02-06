1fbddb7103f3f8a9bde31bd4431712de8cd83443
Dave L2 <liggsie@gmail.com>
2012-02-05 18:19:49 -0800

    Basic functionality now works

--- gridlock.rb
+++ gridlock.rb
@@ -4,39 +4,125 @@ require 'openssl'
 require 'rubygems'
 require 'highline/import'
 
-class GridLock
+module GridLock
 
-  def initialize(service_key)
-    @service_key = service_key
-  end
+  class MasterPasswordError < StandardError; end
 
-  def prompt
-    @master = ask('Enter the master password: ') {|q| q.echo = '*'}
-    if @master.nil? or @master.length == 0
-      puts 'BOOOOOOOOOOOO'
+  class TokenStream
+    HashAlg = 'sha512'
+
+    @@letters = nil
+    def self.letters
+      if @@letters.nil?
+        @@letters = {
+          :cons => %W{b c d f g h j k l m n p q r s t v w x z},
+          :vowels => %W{a e i o u y} 
+        }
+      end
+      @@letters
     end
-    puts "The password is #{@master}"
+
+    def initialize(master, service_key, token_size=4)
+      @master = master
+      @service_key = service_key
+      @index = @master.length + @service_key.length
+      @tokens = Array.new
+      @token_size = token_size
+    end
+
+    def yield_token
+      if @tokens.length < 1
+        populate_token_list
+      end
+      result = @tokens.first
+      @tokens = @tokens[1..-1]
+      result
+    end
+    
+    private
+    def populate_token_list
+      d = OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new(HashAlg), 
+                               @master, 
+                               @service_key + @index.to_s)
+      odd = false
+      intermediate = ''
+      d.each_byte do |b|
+        odd = ! odd
+        if odd
+          list = self.class.letters[:cons]
+        else
+          list = self.class.letters[:vowels]
+        end
+        intermediate << list[b % list.length]
+        if intermediate.length == @token_size
+          @tokens << intermediate
+          intermediate = ''
+        end
+      end
+      @index += 1
+    end
   end
 
-  def display
-    puts 'display'
+  class Manager
+    def initialize(service_key)
+      @service_key = service_key
+      @master = nil
+    end
+
+    def prompt
+      @master = ask('Enter the master password: ') {|q| q.echo = '*'}
+      if @master.nil? or @master.length == 0
+        raise(MasterPasswordError, 'Must choose a master password other than the empty string.')
+      end
+    end
+
+    def self.border_array(grid_size)
+      list = %W{a b c d e f g h i j k l m n o p q r s t u v w x y z}
+      result = Array.new
+      list.each do |letter|
+        result << letter.capitalize * 4
+        if result.length >= grid_size
+          break
+        end
+      end
+      result
+    end
+    
+    Sep = ' | '
+    def display
+      grid = 13
+      token_stream = TokenStream.new(master_pass, @service_key)
+      border_items = self.class.border_array(grid)
+      print '       '
+      puts border_items.join(Sep)
+      border_items.each do |item|
+        print "#{item}#{Sep}"
+        row = Array.new
+        grid.times { row << token_stream.yield_token }
+        a = row.join(Sep)
+        puts a
+      end
+    end
+
+    def master_pass
+      if @master.nil?
+        prompt
+      end
+      @master
+    end
   end
 
-  def master_pass
-    @master ||= prompt
-  end
-end
 
-
-if __FILE__ == $0
-  if ARGV.length < 1
-    error = "
-    Error. Usage:  `#{$0} $SERVICE_NAME_KEY`
-             e.g:  `#{$0} ymail.com`
-    "
-    puts error
-    Kernel.exit(1)
+  if __FILE__ == $0
+    if ARGV.length < 1
+      error = "
+      Error. Usage:  `#{$0} $SERVICE_NAME_KEY`
+               e.g:  `#{$0} ymail.com`
+      "
+      puts error
+      Kernel.exit(1)
+    end
+    g = GridLock::Manager.new(ARGV[-1]).display
   end
-  g = GridLock.new(ARGV[1])
-  puts g.master_pass
+
 end
--- gridlock.spec.rb
+++ gridlock.spec.rb
@@ -2,12 +2,40 @@
 
 require 'gridlock.rb'
 
-describe GridLock do
+describe GridLock::Manager do
   it 'should set the service key and prompt for master password' do
     password = 'my_password'
-    obj = GridLock.new(key)
-    obj.should_receive(:prompt).and_return(password)
+    service = 'ymail.com'
+    obj = GridLock::Manager.new(service)
+    obj.should_receive(:ask).and_return(password)
     obj.master_pass.should == password
+  end
+
+  it 'should raise on receiving an empty password' do
+    service = 'ymail.com'
+    obj = GridLock::Manager.new(service)
+    obj.should_receive(:ask).and_return('')
+    lambda{obj.master_pass}.should raise_error(GridLock::MasterPasswordError)
+  end
+
+end
+
+describe GridLock::TokenStream do
+  it 'should have the correct letters set' do
+    service = 'ymail.com'
+    obj = GridLock::TokenStream.new('password', 'ymail.com')
+    obj.class.letters[:cons].uniq.length.should == 20
+    obj.class.letters[:vowels].uniq.length.should == 6
+  end
+
+  it 'should foo' do
+    obj = GridLock::TokenStream.new('password', 'ymail.com')
+    obj.yield_token
+    obj.yield_token
+  end
+
+  it 'should use SHA512 as the hash algorithm' do
+    GridLock::TokenStream::HashAlg.should == 'sha512'
   end
 end
 
